306198fe20cec23b78c8e6e02924678b1362facf
Randazzz <kir.liaxiwsky2015@yandex.ru>
2025-03-13 15:00:30 +0300

    Рефакторинг

--- src/db/models/utils.py
+++ src/db/models/utils.py
@@ -1,10 +1,12 @@
-from datetime import datetime, timedelta, UTC
+from datetime import UTC, datetime, timedelta
 
 from src.core.config import settings
 
 
 def default_return_utc_datetime() -> datetime:
-    return datetime.now(UTC).replace(tzinfo=None) + timedelta(days=settings.BOOK_LOAN_DAYS)
+    return datetime.now(UTC).replace(tzinfo=None) + timedelta(
+        days=settings.BOOK_LOAN_DAYS
+    )
 
 
 def get_current_utc_datetime() -> datetime:
