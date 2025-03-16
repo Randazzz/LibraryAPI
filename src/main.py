5440f7d84afadadfc7d445c8648c39914afd8fe8
Randazzz <kir.liaxiwsky2015@yandex.ru>
2025-03-16 17:24:44 +0300

    Добавлена обработка исключения если ошибка подключения к redis

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import logging
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from redis import RedisError
 from redis import asyncio as aioredis
 
 from src.api.v1 import router as v1_router
@@ -13,10 +15,22 @@ from src.core.logging import setup_logging
 
 setup_logging()
 
+logger = logging.getLogger(__name__)
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url(settings.REDIS_URL)
+    try:
+        redis = aioredis.from_url(
+            settings.REDIS_URL, encoding="utf8", decode_responses=True
+        )
+        await redis.ping()
+        logger.info("Successfully connected to Redis!")
+    except RedisError as e:
+        logger.error(
+            f"Failed to connect to Redis: {type(e).__name__}: {str(e)}"
+        )
+        raise RuntimeError(f"Redis connection error: {e}")
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
     yield
     await redis.close()
